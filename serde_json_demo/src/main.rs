8f168df2d6e81ce592d3970f9f8987ac5a6a2f2d
nasoundead <8845497+nasoundead@user.noreply.gitee.com>
2022-07-31 13:52:48 +0800

    f2

---
+++ serde_json_demo/src/main.rs
@@ -0,0 +1,135 @@
+use serde::{Deserialize, Serialize};
+// use serde_json::Result;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Person {
+    name: String,
+    age: u8,
+    phones: Vec<String>,
+}
+
+fn main() {
+    let point = Point { x: 1, y: 2 };
+
+    // Convert the Point to a JSON string.
+    let serialized = serde_json::to_string(&point).unwrap();
+
+    // Prints serialized = {"x":1,"y":2}
+    println!("serialized = {}", serialized);
+
+    // Convert the JSON string back to a Point.
+    let deserialized: Point = serde_json::from_str(&serialized).unwrap();
+
+    // Prints deserialized = Point { x: 1, y: 2 }
+    println!("deserialized = {:?}", deserialized);
+}
+
+#[cfg(test)]
+mod tests {
+    // Constructing JSON values
+    // Serde JSON provides a json! macro to build serde_json::Value objects with very natural JSON syntax.
+    use serde_json::json;
+
+    #[test]
+    fn constructing_json_values() {
+        let full_name = "John Doe";
+        let age_last_year = 42;
+        // The type of `john` is `serde_json::Value`
+        let john = json!(
+            {
+                "name": full_name,
+                "age": age_last_year + 1,
+                "phones": [
+                    "+44 1234567",
+                    "+44 2345678"
+                ]
+            }
+        );
+        println!("{}", john);
+        println!("{}", john["age"]);
+        println!("first phone number: {}", john["phones"][0]);
+        // Convert to a string of JSON and print it out
+        println!("{}", john.to_string());
+    }
+
+    // Operating on untyped JSON values
+    // Any valid JSON data can be manipulated in the following recursive enum representation.
+    // This data structure is serde_json::Value.
+
+    // enum Value {
+    //     Null,
+    //     Bool(bool),
+    //     Number(Number),
+    //     String(String),
+    //     Array(Vec<Value>),
+    //     Object(Map<String, Value>),
+    // }
+    // A string of JSON data can be parsed into a serde_json::Value by the serde_json::from_str function.
+    // There is also from_slice for parsing from a byte slice &[u8] and
+    // from_reader for parsing from any io::Read like a File or a TCP stream.
+
+    use serde_json::{Result, Value};
+
+    #[test]
+    fn untyped_example() -> Result<()> {
+        // Some JSON input data as a &str. Maybe this comes from the user.
+        let data = r#"
+            {
+                "name": "John Doe",
+                "age": 43,
+                "phones": [
+                    "+44 1234567",
+                    "+44 2345678"
+                ]
+            }"#;
+
+        // Parse the string of data into serde_json::Value.
+        let v: Value = serde_json::from_str(data)?;
+
+        // Access parts of the data by indexing with square brackets.
+        println!("Please call {} at the number {}", v["name"], v["phones"][0]);
+
+        Ok(())
+    }
+
+    // Parsing JSON as strongly typed data structures
+    // Serde provides a powerful way of mapping JSON data into Rust data structures largely automatically.
+    use serde::{Deserialize, Serialize};
+    
+
+    #[derive(Serialize, Deserialize)]
+    struct Person {
+        name: String,
+        age: u8,
+        phones: Vec<String>,
+    }
+
+    #[test]
+    fn typed_example() -> Result<()> {
+        // Some JSON input data as a &str. Maybe this comes from the user.
+        let data = r#"
+            {
+                "name": "John Doe",
+                "age": 43,
+                "phones": [
+                    "+44 1234567",
+                    "+44 2345678"
+                ]
+            }"#;
+
+        // Parse the string of data into a Person object. This is exactly the
+        // same function as the one that produced serde_json::Value above, but
+        // now we are asking it for a Person as output.
+        let p: Person = serde_json::from_str(data)?;
+
+        // Do things just like with any other Rust data structure.
+        println!("Please call {} at the number {}", p.name, p.phones[0]);
+        Ok(())
+    }
+}
---
+++ vec_demo/src/main.rs
@@ -0,0 +1,102 @@
+#[derive(Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Drop for Point {
+    fn drop(&mut self) {
+        println!("Point({}, {}) dropped", self.x, self.y);
+    }
+}
+fn main() {
+    let p = &Point { x: 0, y: 0 };
+
+    println!("{:?}", p);
+    println!("{p:#?}");
+
+    let a_vec : Vec<String> = vec![];
+    if a_vec.is_empty() {
+        println!("nothing in there!");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // Moves all the elements of other into Self, leaving other empty.
+    // From your example, the following code will concatenate two vectors by mutating a and b:
+    #[test]
+    fn test_append() {
+        let mut a = vec![1, 2, 3];
+        let mut b = vec![4, 5, 6];
+
+        a.append(&mut b);
+        assert_eq!(a, [1, 2, 3, 4, 5, 6]);
+        assert_eq!(b, []);
+    }
+
+    #[test]
+    fn test_extend() {
+        let mut a = vec![1, 2, 3];
+        let b = vec![4, 5, 6];
+
+        a.extend(b);
+        assert_eq!(a, [1, 2, 3, 4, 5, 6]);
+        // b is moved can't be used anymore
+        // assert_eq!(b, [4, 5, 6]);
+    }
+
+    // Note that the vector b is moved instead of emptied.
+    // If your vectors contain elements that implement Copy,
+    // you can pass an immutable reference to one vector to extend() instead in order to avoid the move.
+    // In that case the vector b is not changed:
+    #[test]
+    fn test_extend_ref() {
+        let mut a = vec![1, 2, 3];
+        let b = vec![4, 5, 6];
+
+        a.extend(&b);
+        assert_eq!(a, [1, 2, 3, 4, 5, 6]);
+        assert_eq!(b, [4, 5, 6]);
+    }
+
+    #[test]
+    fn test_chain_consumed() {
+        let a = vec![1, 2, 3];
+        let b = vec![4, 5, 6];
+
+        let c: Vec<i32> = a.into_iter().chain(b.into_iter()).collect(); // Consumed
+        // println!("Consumed {:?}", a);
+        assert_eq!(c, [1, 2, 3, 4, 5, 6]);
+    }
+
+    #[test]
+    fn test_chain_referenced() {
+        let a = vec![1, 2, 3];
+        let b = vec![4, 5, 6];
+
+        let mut c: Vec<&i32> = a.iter().chain(b.iter()).collect(); // Referenced
+        assert_eq!(c, [&1, &2, &3, &4, &5, &6]);
+        c[0] = &100;
+        println!("a: {a:?}");
+        println!("c: {c:?}");
+    }
+
+    #[test]
+    fn test_chain_cloned() {
+        let a = vec![1, 2, 3];
+        let b = vec![4, 5, 6];
+
+        let c: Vec<i32> = a.iter().cloned().chain(b.iter().cloned()).collect(); // Cloned
+        assert_eq!(c, [1, 2, 3, 4, 5, 6]);
+    }
+
+    #[test]
+    fn test_chain_copied() {
+        let a = vec![1, 2, 3];
+        let b = vec![4, 5, 6];
+
+        let c: Vec<i32> = a.iter().copied().chain(b.iter().copied()).collect(); // Copied
+        assert_eq!(c, [1, 2, 3, 4, 5, 6]);
+    }
+}
